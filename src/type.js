82bb1c1f9dced70e5d97e49360ce749f8054cf3c
aim-leo <luozewei123@qq.com>
2021-03-24 15:29:25 +0800

    feat: add id type

--- src/type.js
+++ src/type.js
@@ -35,4 +35,15 @@ function defineType(alias, t) {
 // add a alias for function
 defineType('func', type.function)
 
+// mongodb objectid
+// not a strict validator, you can replace it by mongoose.Types.ObjectId.isValid
+defineType('id', () => type.string().clone().convert().addValidator({
+  name: 'MongoObjectIdValidator',
+  validator: val => {
+    const objIdPattern = /^[0-9a-fA-F]{24}$/;
+    return (Boolean(val) && !Array.isArray(val) && objIdPattern.test(String(val)))
+  },
+  message: 'expect a valid mongo object id'
+}))
+
 module.exports = type
